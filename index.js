ddc0035ff456995c4702f2a178d8ad33f4027fc1
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-10-05 16:54:56 +0800

    github migrate

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+// This is your real test secret API key.
+const stripe = require("stripe")("sk_test_51JgYIxKPfEZYnk5WtRMNHEACMLYB97y2ooZv2shw7ACfIgks8jZHwgjhP1Panvq7sGMri1Odfk2GWUleRyzFSbBf00jL9I8Ufq");
+app.use(express.static("public"));
+app.use(express.json());
+const calculateOrderAmount = items => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return items[0].amount;
+};
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  const { currency } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: currency
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
+});
+app.listen(process.env.PORT, () => console.log('Node server listening!'));
